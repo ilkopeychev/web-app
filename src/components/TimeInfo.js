b2e95d6e3f13ce2521c44d9a2d7eb601254bf362
ilkopeychev <ilkokpeychev@gmail.com>
2022-09-27 16:41:00 +0300

    test ui changes

--- src/components/TimeInfo.js
+++ src/components/TimeInfo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-function TimeInfo({ siteName, windSpeed, activePower }) {
+function TimeInfo({ post }) {
+  console.log(post);
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <div>{siteName}</div>
-      <div>{windSpeed}</div>
-      <ProgressBar completed={activePower} />
+      <div>{post.siteName}</div>
+      <div>{post.windSpeed}</div>
+      <ProgressBar completed={post.activePower} />
     </div>
   );
 }
